7b5e95399e726a1cdf5b4f665b6778b4fa397629
Jinu Nyachhyon <nyachhyonjinu@gmail.com>
2023-09-24 11:56:44 +0545

    GPU inference script

---
+++ Quantizing-Compiling-YOLOv3-Pytorch with DPU Inference/GPU inference/gpu_inference.py
@@ -0,0 +1,135 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import cv2
+import importlib
+import random
+from nets.model_main import ModelMain
+from gpu_utils import YOLOPost, non_max_suppression
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+####### Load the model -- config , data parallel, restore pretrain model
+params_path = "params.py"
+config = importlib.import_module(params_path[:-3]).TRAINING_PARAMS
+
+model = ModelMain(config, is_training=False)
+model.train(False)
+
+# Set data parallel
+model = nn.DataParallel(model)
+model = model.to(device)
+
+# Restore pretrain model
+if config["pretrain_snapshot"]:
+    state_dict = torch.load(config["pretrain_snapshot"], map_location=torch.device('cpu'))
+    model.load_state_dict(state_dict)
+else:
+    raise Exception("missing pretrain_snapshot!!!")
+
+# print(model)
+
+
+# YOLO loss with 3 scales
+yolo_losses = []
+for i in range(3):
+    # yolo_losses.append(YOLOLoss(config["yolo"]["anchors"][i],
+    #                             config["yolo"]["classes"], (config["img_w"], config["img_h"])))
+
+    yolo_losses.append(YOLOPost(config["yolo"]["anchors"][i],
+                                config["yolo"]["classes"], (config["img_w"], config["img_h"])))
+
+
+
+####### Inference
+
+# Pre-processing
+image_path = "test/images/test1.jpg"
+image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+image = cv2.resize(image, (config["img_w"], config["img_h"]),
+                               interpolation=cv2.INTER_LINEAR)
+image = image.astype(np.float32)
+image /= 255.0
+image = np.transpose(image, (2, 0, 1))
+image = image.astype(np.float32)
+
+# print(image.shape) # (3,416,416)
+
+image = torch.from_numpy(image)
+image = image.unsqueeze(0)
+
+# print(image.shape) # ([1,3,416,416])
+
+
+####### Inference
+
+# Perform inference
+with torch.no_grad():
+    out = model(image.to(device))
+
+# print(out[0].shape) # ([1,255,13,13]) -> torch
+
+# Convert tensor to numpy
+output = []
+output.append(out[0].numpy())
+output.append(out[1].numpy())
+output.append(out[2].numpy())
+# print(len(output))
+# print(output[0].shape) # (1, 255, 13, 13) -> numpy
+
+output_list = []
+for i in range(3):
+    output_list.append(yolo_losses[i].forward(output[i]))
+output_con = np.concatenate(output_list, 1)
+
+
+# print(len(output_list))
+# print(output_list[0].shape) # ([1, 507, 85]) | 13*13*3
+# print(output_list[1].shape) # ([1, 2028, 85]) | 26*26*3
+# print(output_list[2].shape) # ([1, 8112, 85]) | 52*52*3
+
+# print(len(output_con))
+# print(output_con.shape) # ([1, 10647, 85])
+
+batch_detections = non_max_suppression(output_con, config["yolo"]["classes"],
+                                                   conf_thres=config["confidence_threshold"],
+                                                   nms_thres=0.45)
+
+print(batch_detections)
+
+
+######## Plot prediction with bounding box
+classes = open(config["classes_names_path"], "r").read().split("\n")[:-1]
+# print(classes)
+
+for idx, detections in enumerate(batch_detections):
+    if detections is not None:
+        im = cv2.imread(image_path)
+        # print(im.shape) # eg. (428, 640, 3)
+        unique_labels = np.unique(detections[:, -1])
+        n_cls_preds = len(unique_labels)
+        bbox_colors = {int(cls_pred): (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) for cls_pred in unique_labels}
+        
+        for x1, y1, x2, y2, conf, cls_conf, cls_pred in detections:
+            color = bbox_colors[int(cls_pred)]
+
+            # Rescale coordinates to original dimensions
+            ori_h, ori_w, _ = im.shape
+            pre_h, pre_w = config["img_h"], config["img_w"]
+            box_h = ((y2 - y1) / pre_h) * ori_h
+            box_w = ((x2 - x1) / pre_w) * ori_w
+            y1 = (y1 / pre_h) * ori_h
+            x1 = (x1 / pre_w) * ori_w
+
+            # Create a Rectangle patch
+            cv2.rectangle(im, (int(x1), int(y1)), (int(x1 + box_w), int(y1 + box_h)), color, 2)
+
+            # Add label
+            label = classes[int(cls_pred)]
+            cv2.putText(im, label, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+    # Save generated image with detections
+    output_path = 'prediction_gpu_0.2.jpg'
+    cv2.imwrite(output_path, im)
---
+++ Quantizing-Compiling-YOLOv3-Pytorch with DPU Inference/common/coco_dataset.py
@@ -0,0 +1,84 @@
+import os
+import numpy as np
+import logging
+import cv2
+
+import torch
+from torch.utils.data import Dataset
+
+from . import data_transforms
+
+
+class COCODataset(Dataset):
+    def __init__(self, list_path, img_size, is_training, is_debug=False):
+        self.img_files = []
+        self.label_files = []
+        for path in open(list_path, 'r'):
+            label_path = path.replace('images', 'labels').replace('.png', '.txt').replace(
+                '.jpg', '.txt').strip()
+            if os.path.isfile(label_path):
+                self.img_files.append(path)
+                self.label_files.append(label_path)
+            else:
+                logging.info("no label found. skip it: {}".format(path))
+        logging.info("Total images: {}".format(len(self.img_files)))
+        self.img_size = img_size  # (w, h)
+        self.max_objects = 50
+        self.is_debug = is_debug
+
+        #  transforms and augmentation
+        self.transforms = data_transforms.Compose()
+        if is_training:
+            self.transforms.add(data_transforms.ImageBaseAug())
+        # self.transforms.add(data_transforms.KeepAspect())
+        self.transforms.add(data_transforms.ResizeImage(self.img_size))
+        self.transforms.add(data_transforms.ToTensor(self.max_objects, self.is_debug))
+
+    def __getitem__(self, index):
+        img_path = self.img_files[index % len(self.img_files)].rstrip()
+        img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+        if img is None:
+            raise Exception("Read image error: {}".format(img_path))
+        ori_h, ori_w = img.shape[:2]
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        label_path = self.label_files[index % len(self.img_files)].rstrip()
+        if os.path.exists(label_path):
+            labels = np.loadtxt(label_path).reshape(-1, 5)
+        else:
+            logging.info("label does not exist: {}".format(label_path))
+            labels = np.zeros((1, 5), np.float32)
+
+        sample = {'image': img, 'label': labels}
+        if self.transforms is not None:
+            sample = self.transforms(sample)
+        sample["image_path"] = img_path
+        sample["origin_size"] = str([ori_w, ori_h])
+        return sample
+
+    def __len__(self):
+        return len(self.img_files)
+
+
+#  use for test dataloader
+if __name__ == "__main__":
+    dataloader = torch.utils.data.DataLoader(COCODataset("../data/coco/trainvalno5k.txt",
+                                                         (416, 416), True, is_debug=True),
+                                             batch_size=2,
+                                             shuffle=False, num_workers=1, pin_memory=False)
+    for step, sample in enumerate(dataloader):
+        for i, (image, label) in enumerate(zip(sample['image'], sample['label'])):
+            image = image.numpy()
+            h, w = image.shape[:2]
+            for l in label:
+                if l.sum() == 0:
+                    continue
+                x1 = int((l[1] - l[3] / 2) * w)
+                y1 = int((l[2] - l[4] / 2) * h)
+                x2 = int((l[1] + l[3] / 2) * w)
+                y2 = int((l[2] + l[4] / 2) * h)
+                cv2.rectangle(image, (x1, y1), (x2, y2), (0, 0, 255))
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            cv2.imwrite("step{}_{}.jpg".format(step, i), image)
+        # only one batch
+        break
