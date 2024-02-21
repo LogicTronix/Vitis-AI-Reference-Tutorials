7c3356f924fe39e0f8f3110547aeb15ebec4e698
Jinu Nyachhyon <nyachhyonjinu@gmail.com>
2024-02-21 17:28:42 +0545

    Renamed for consistency

--- Quantizing-Resnet-Variants/ResNet152/qat_inference.py
+++ Quantizing-Resnet-Variants/ResNet152/qat_inference.py
@@ -8,7 +8,7 @@ import pytorch_nndct
 
 def predict(image):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = torch.jit.load('qat_result/ResNet_int.pt', map_location=torch.device('cpu'))
+    model = torch.jit.load('qat_result/ResNet_int.pt', map_location=torch.device(device))
 
     model.eval()
     model = model.to(device)
