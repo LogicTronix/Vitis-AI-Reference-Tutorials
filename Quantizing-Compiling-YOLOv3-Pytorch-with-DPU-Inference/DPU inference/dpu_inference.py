fd0747d688dbc671ef57c0ce9baa7e973cbe65e1
Jinu Nyachhyon <nyachhyonjinu@gmail.com>
2024-02-23 01:12:38 +0545

    Remove unnecessary print statements for improved performance and enhance readability of utils files

--- Quantizing-Compiling-YOLOv3-Pytorch-with-DPU-Inference/DPU inference/dpu_inference.py
+++ Quantizing-Compiling-YOLOv3-Pytorch-with-DPU-Inference/DPU inference/dpu_inference.py
@@ -15,7 +15,6 @@ from dpu_utils import YOLOPost, non_max_suppression
 def runYolo(dpu_runner_tfYolo, image, config, image_path):
     config = config
 
-    print("inside the run yolo..")
     inputTensors = dpu_runner_tfYolo.get_input_tensors()  #  get the model input tensor
     outputTensors = dpu_runner_tfYolo.get_output_tensors() # get the model ouput tensor
     
@@ -32,16 +31,12 @@ def runYolo(dpu_runner_tfYolo, image, config, image_path):
     outputChannel_2 = outputTensors[2].dims[3]    
 
     outputSize_0 = (outputHeight_0,outputWidth_0,outputChannel_0)
-    print("outputSize_0 ", outputSize_0) #(13, 13, 255)
     outputSize_1 = (outputHeight_1,outputWidth_1,outputChannel_1)
-    print("outputSize_1 ", outputSize_1) #(26, 26, 255)
     outputSize_2 = (outputHeight_2,outputWidth_2,outputChannel_2)
-    print("outputSize_2 ", outputSize_2) #(52, 52, 255)
+
 
     runSize = 1
     shapeIn = (runSize,) + tuple([inputTensors[0].dims[i] for i in range(inputTensors[0].ndim)][1:])
-    print("What shapeIn", shapeIn) # (1,416,416,3)
-    print("InputTensor[0]: ", inputTensors[0])
 
     # InputTensor[0]: {name: 'ModelMain__input_0_fix', shape: [1, 416, 416, 3], type: 'xint8', attrs: {'location': 1, 'ddr_addr': 1264, 'bit_width': 8, 'round_mode': 'DPU_ROUND', 'reg_id': 2, 'fix_point': 4, 'if_signed': True}}
 
@@ -57,24 +52,15 @@ def runYolo(dpu_runner_tfYolo, image, config, image_path):
     imageRun = inputData[0]
     imageRun[0,...] = image.reshape(inputTensors[0].dims[1],inputTensors[0].dims[2],inputTensors[0].dims[3])
 
-
-    print("Execute async")
+    '''Execute Async'''
     job_id = dpu_runner_tfYolo.execute_async(inputData,outputData)
     dpu_runner_tfYolo.wait(job_id)
-    print("Execcution completed..")
 
-    print()
-    print("Shapes od output: ")
-    print(outputData[0].shape) # (1, 13, 13, 255)
-    print(outputData[1].shape) # (1, 26, 26, 255)
-    print(outputData[2].shape) # (1, 52, 52, 255)
-    print()
-    print("Input image shape: ", inputData[0].shape) # (1,416,416,3)
-    print("Image shape[0]: ", inputData[0][0].shape) # (416,416,3)
-
+    '''Transpose Output for consistency'''
     outputData[0] = np.transpose(outputData[0], (0,3,1,2))
     outputData[1] = np.transpose(outputData[1], (0,3,1,2))
     outputData[2] = np.transpose(outputData[2], (0,3,1,2))
+
 
     '''Post Processing'''
     # YOLO loss with 3 scales
@@ -88,29 +74,20 @@ def runYolo(dpu_runner_tfYolo, image, config, image_path):
         output_list.append(yolo_losses[i].forward(outputData[i]))
     output_con = np.concatenate(output_list, 1)
 
-    print(len(output_list))
-    print(output_list[0].shape) # ([1, 507, 85]) | 13*13*3
-    print(output_list[1].shape) # ([1, 2028, 85]) | 26*26*3
-    print(output_list[2].shape) # ([1, 8112, 85]) | 52*52*3
 
-    print(len(output_con))
-    print(output_con.shape) # ([1, 10647, 85])
-
+    # Perform Non Max Supression
     batch_detections = non_max_suppression(output_con, config["yolo"]["classes"],
                                                    conf_thres=config["confidence_threshold"],
                                                    nms_thres=0.45)
-
     print(batch_detections)
 
 
     '''Plot prediction with bounding box'''
     classes = open(config["classes_names_path"], "r").read().split("\n")[:-1]
-    # print(classes)
 
     for idx, detections in enumerate(batch_detections):
         if detections is not None:
             im = cv2.imread(image_path)
-            print(im.shape) # eg. (428, 640, 3)
             unique_labels = np.unique(detections[:, -1])
             n_cls_preds = len(unique_labels)
             bbox_colors = {int(cls_pred): (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) for cls_pred in unique_labels}
@@ -172,7 +149,7 @@ def main(argv):
 
     """Creates DPU runner, associated with the DPU subgraph."""
     dpu_runners = vart.Runner.create_runner(subgraphs[0], "run")
-    print("DPU Runner Created")
+
 
     # Get config
     params_path = "params.py"
@@ -186,20 +163,13 @@ def main(argv):
                                 interpolation=cv2.INTER_LINEAR)
     image = image.astype(np.float32)
     image /= 255.0
-    # image = np.transpose(image, (2, 0, 1))
-    # image = image.astype(np.float32) # (3,416,416)
-    print(image.shape)
-    print("Preprocessed image")
 
     # Measure time 
     time_start = time.time()
 
     """Assigns the runYolo function with corresponding arguments"""
-    print("runYolo -- main function intialize")
     runYolo(dpu_runners, image, config, image_path)
-
     del dpu_runners
-    print("DPU runnerr deleted")
 
     time_end = time.time()
     timetotal = time_end - time_start
--- Quantizing-Compiling-YOLOv3-Pytorch-with-DPU-Inference/DPU inference/dpu_utils.py
+++ Quantizing-Compiling-YOLOv3-Pytorch-with-DPU-Inference/DPU inference/dpu_utils.py
@@ -1,13 +1,18 @@
 import numpy as np
 
 def sigmoid(x):
+    """Apply sigmoid activation function."""
     return 1 / (1 + np.exp(-x))
 
 def exp(x):
+    """Compute exponential."""
     return np.exp(x)
 
 class YOLOPost:
+    """Class for YOLO post-processing."""
+
     def __init__(self, anchors, num_classes, img_size):
+        """Initialize YOLOPost."""
         self.anchors = anchors
         self.num_anchors = len(anchors)
         self.num_classes = num_classes
@@ -21,21 +26,16 @@ class YOLOPost:
         self.lambda_cls = 1.0
 
     def forward(self, input, targets=None):
+        """Perform forward pass."""
         bs = input.shape[0]
         in_h = input.shape[2]
         in_w = input.shape[3]
         stride_h = self.img_size[1] / in_h
         stride_w = self.img_size[0] / in_w
-        # scaled_anchors = np.array([[a_w / stride_w, a_h / stride_h] for a_w, a_h in self.anchors])
-
+  
         scaled_anchors = [(a_w / stride_w, a_h / stride_h) for a_w, a_h in self.anchors]
 
-
-        # prediction = input.view(bs,  self.num_anchors,
-        #                         self.bbox_attrs, in_h, in_w).permute(0, 1, 3, 4, 2).contiguous()
-        
         prediction = input.reshape(bs, self.num_anchors, self.bbox_attrs, in_h, in_w).transpose(0, 1, 3, 4, 2)
-        print("Prediction shape: ", prediction.shape)
 
         # Get outputs
         x = sigmoid(prediction[..., 0])          # Center x
@@ -45,23 +45,12 @@ class YOLOPost:
         conf = sigmoid(prediction[..., 4])       # Conf
         pred_cls = sigmoid(prediction[..., 5:])  # Cls pred.
 
-
-        print("forward being called")
-
         # Calculate offsets for each grid
-        # grid_x = torch.linspace(0, in_w-1, in_w).repeat(in_w, 1).repeat(
-        #     bs * self.num_anchors, 1, 1).view(x.shape).type(FloatTensor)
-        # grid_y = torch.linspace(0, in_h-1, in_h).repeat(in_h, 1).t().repeat(
-        #     bs * self.num_anchors, 1, 1).view(y.shape).type(FloatTensor)
         grid_x = np.tile(np.tile(np.linspace(0, in_w-1, in_w), (in_w, 1)), (bs * self.num_anchors, 1, 1)).reshape(x.shape)
         grid_y = np.tile(np.tile(np.linspace(0, in_h-1, in_h), (in_h, 1)).T, (bs * self.num_anchors, 1, 1)).reshape(y.shape)
 
 
         # Calculate anchor w, h
-        # anchor_w = FloatTensor(scaled_anchors).index_select(1, LongTensor([0]))
-        # anchor_h = FloatTensor(scaled_anchors).index_select(1, LongTensor([1]))
-        # anchor_w = anchor_w.repeat(bs, 1).repeat(1, 1, in_h * in_w).view(w.shape)
-        # anchor_h = anchor_h.repeat(bs, 1).repeat(1, 1, in_h * in_w).view(h.shape)
         anchor_w = np.array(scaled_anchors)[:, 0].reshape(1, self.num_anchors, 1, 1)
         anchor_h = np.array(scaled_anchors)[:, 1].reshape(1, self.num_anchors, 1, 1)
         anchor_w = np.tile(anchor_w, (bs, 1, in_h, in_w))
@@ -77,11 +66,8 @@ class YOLOPost:
 
 
         # Results
-        # _scale = torch.Tensor([stride_w, stride_h] * 2).type(FloatTensor)
         _scale = np.array([stride_w, stride_h] * 2)
 
-        # output = torch.cat((pred_boxes.view(bs, -1, 4) * _scale,
-        #                     conf.view(bs, -1, 1), pred_cls.view(bs, -1, self.num_classes)), -1)
         output = np.concatenate((
             (pred_boxes.reshape(bs, -1, 4) * _scale),
             conf.reshape(bs, -1, 1),
@@ -132,8 +118,7 @@ def non_max_suppression(prediction, num_classes, conf_thres=0.5, nms_thres=0.4):
     """
 
     # From (center x, center y, width, height) to (x1, y1, x2, y2)
-    
-    # box_corner = prediction.new(prediction.shape)
+
     box_corner = np.copy(prediction)
 
     box_corner[:, :, 0] = prediction[:, :, 0] - prediction[:, :, 2] / 2
@@ -145,10 +130,9 @@ def non_max_suppression(prediction, num_classes, conf_thres=0.5, nms_thres=0.4):
     output = [None for _ in range(len(prediction))]
 
     for image_i, image_pred in enumerate(prediction):
-        print("Entered NMS loop!!")
 
         # Filter out confidence scores below threshold
-        conf_mask = (image_pred[:, 4] >= conf_thres).squeeze()
+        conf_mask = (image_pred[:, 4] >= conf_thres)
         image_pred = image_pred[conf_mask]
         print(image_pred.shape) # (17, 85)
 
@@ -157,27 +141,24 @@ def non_max_suppression(prediction, num_classes, conf_thres=0.5, nms_thres=0.4):
             continue
             
         # Get score and class with highest confidence
-        # class_conf, class_pred = torch.max(image_pred[:, 5:5 + num_classes], 1,  keepdim=True)
         class_conf = np.max(image_pred[:, 5:5 + num_classes], axis=1, keepdims=True)
+
         class_pred = np.argmax(image_pred[:, 5:5 + num_classes], axis=1)
         class_pred = class_pred[:, np.newaxis]  # Add a new axis to mimic keepdims=True
 
-        # print(class_pred, class_conf)
 
         # Detections ordered as (x1, y1, x2, y2, obj_conf, class_conf, class_pred)
-        # detections = torch.cat((image_pred[:, :5], class_conf.float(), class_pred.float()), 1)
         detections = np.concatenate((image_pred[:, :5], class_conf.astype(np.float32), class_pred.astype(np.float32)), axis=1)
 
         # Iterate through all predicted classes
         unique_labels = np.unique(detections[:, -1])
+
 
         for c in unique_labels:
             # Get the detections with the particular class
             detections_class = detections[detections[:, -1] == c]
 
             # Sort the detections by maximum objectness confidence
-            # _, conf_sort_index = torch.sort(detections_class[:, 4], descending=True)
-            # detections_class = detections_class[conf_sort_index]
             conf_sort_index = np.argsort(detections_class[:, 4])[::-1]
             detections_class = detections_class[conf_sort_index]
 
@@ -185,7 +166,6 @@ def non_max_suppression(prediction, num_classes, conf_thres=0.5, nms_thres=0.4):
             max_detections = []
             while detections_class.shape[0]:
                 # Get detection with highest confidence and save as max detection
-                # max_detections.append(detections_class[0].unsqueeze(0))
                 max_detections.append(detections_class[0].reshape(1, -1))
 
                 # Stop if we're at the last detection
@@ -194,9 +174,6 @@ def non_max_suppression(prediction, num_classes, conf_thres=0.5, nms_thres=0.4):
 
                 # Get the IOUs for all boxes with lower confidence
                 ious = bbox_iou(max_detections[-1], detections_class[1:])
-
-                # Remove detections with IoU >= NMS threshold
-                # detections_class = detections_class[1:][ious < nms_thres]
 
                 # Find indices of detections with IoU < NMS threshold
                 keep_indices = np.where(ious < nms_thres)[0]
@@ -207,7 +184,6 @@ def non_max_suppression(prediction, num_classes, conf_thres=0.5, nms_thres=0.4):
             max_detections = np.concatenate(max_detections, axis=0)
 
             # Add max detections to outputs
-            # output[image_i] = max_detections if output[image_i] is None else torch.cat((output[image_i], max_detections))
             if output[image_i] is None:
                 output[image_i] = max_detections
             else:
